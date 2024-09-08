93c595e0bf0ffe60985b23ae6c50fc5717c7f8f1
Viptrading8585 <viptrading8888@gmail.com>
2024-09-08 19:48:39 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/ggop/loop.py",
+    "postStartCommand": "python3 /workspaces/ggop/venom.py",
     "customizations": {
         "vscode": {
             "settings": {
