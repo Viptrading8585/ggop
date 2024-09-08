14beed6ab46e221854892a47d33ce9c22a238614
Viptrading8585 <viptrading8888@gmail.com>
2024-09-08 19:47:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7501771631:AAG6Mz_BZ4Ixo6-0F0yZjEpzINarAF4NfQc")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("vishal_8585")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1745945223")
 
 
 
